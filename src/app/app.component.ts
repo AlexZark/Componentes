4a8e73aa49216dbadf469ca2c130886b47a8354a
AlexZark <115591436+AlexZark@users.noreply.github.com>
2023-07-22 09:09:34 -0600

    Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'componentes';
+  options: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+  notificationCount: number = 5; // Aquí defines la propiedad notificationCount con su valor inicial
+
+  constructor(private bottomSheet: MatBottomSheet) {}
+
+  openBottomSheet(): void {
+    this.bottomSheet.open(BottomSheetComponent);
+  }
+  selectedOption: string = 'option1'; // Opción predeterminada seleccionada
+  onToggleChange(event: MatButtonToggleChange) {
+    const selectedValue = event.value;
+    // Aquí puedes manejar la selección realizada por el usuario
+    console.log('Seleccionado:', selectedValue);
+  }
+
+  listItems = [
+    {
+      name: 'Elemento 1',
+      checked: false, // Agregar la propiedad 'checked' y establecerla como false
+      subItems: [
+        { name: 'Sub Elemento 1.1', checked: false },
+        { name: 'Sub Elemento 1.2', checked: false },
+      ]
+    },
+    {
+      name: 'Elemento 2',
+      checked: false, // Agregar la propiedad 'checked' y establecerla como false
+      subItems: [
+        { name: 'Sub Elemento 2.1', checked: false },
+        { name: 'Sub Elemento 2.2', checked: false },
+      ]
+    },
+    // Agregar más elementos y sub-checkboxes según sea necesario
+  ];
+  removeChip() {
+    console.log('Chip removed!');
+    // Aquí puedes agregar la lógica para eliminar el chip de tu lista
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,54 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core'; 
+
+
+import {  ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomSheetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatBottomSheetModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
